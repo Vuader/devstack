451d79004864c7d2737a2a7f3d40f92f619ecbeb
Dave Kruger <Vuader@users.noreply.github.com>
2018-11-09 08:03:40 +0200

    Updated github urls

--- resources/photonic/testdrive_photonic.sh
+++ resources/photonic/testdrive_photonic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -L -O https://raw.githubusercontent.com/TachyonicProject/devstack/development/resources/photonic/resources/Dockerfile
-curl -L -O https://raw.githubusercontent.com/TachyonicProject/devstack/development/resources/photonic/resources/000-default.conf
+curl -L -O https://github.com/TachyonicProject/devstack/raw/development/resources/photonic/Dockerfile
+curl -L -O https://github.com/TachyonicProject/devstack/raw/development/resources/photonic/000-default.conf
 
 docker build -t photonic:dev .
 docker run -p 80:80 photonic:dev &
