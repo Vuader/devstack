97dc2fd48ae794711b0e0420fc56a58553edbf98
Christiaan Rademan <christiaan.rademan@gmail.com>
2018-11-27 15:46:14 +0200

    First working docker dev environment

--- resources/photonic/testdrive_photonic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -L -O https://github.com/TachyonicProject/devstack/raw/development/resources/photonic/Dockerfile
-curl -L -O https://github.com/TachyonicProject/devstack/raw/development/resources/photonic/000-default.conf
-
-docker build -t photonic:dev .
-docker run -p 80:80 photonic:dev &
-
-python -m webbrowser "http://localhost/ui"
